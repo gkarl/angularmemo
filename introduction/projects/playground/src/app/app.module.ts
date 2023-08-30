144818f96c49b3b27a63ac884b59040e73036e1b
Karl Gavillot <ksgavillot@hotmail.com>
2023-08-30 17:13:44 +0200

    Section 3 - 26 Cycle de vie : ngOnInit

--- introduction/projects/playground/src/app/app.module.ts
+++ introduction/projects/playground/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HighlightDirective25 } from './section3DirectivesAttributs/InputRecupererInfoDansAtttributElementHTML25/highlight25.directive';
+import { HighlightDirective26 } from './section3DirectivesAttributs/CycleDeVieNgOnInit26/highlight26.directive';
 
 @NgModule({
-  declarations: [AppComponent, HighlightDirective25],
+  declarations: [AppComponent, HighlightDirective26],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
